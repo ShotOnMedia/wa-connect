640032ffe3716f59906b6098cb341f758ff8297d
ShotOnMedia <shotonmedia@gmail.com>
2026-08-21 22:42:43 +0200

    Register contact custom fields routes

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from app.api.auth import router as auth_router
+from app.api.contact_fields import router as contact_fields_router
 from app.api.contacts import router as contacts_router
 from app.api.conversations import router as conversations_router
 from app.api.settings import router as settings_router
@@ -13,5 +14,6 @@ api_router.include_router(auth_router)
 api_router.include_router(webhooks_router)
 api_router.include_router(conversations_router, dependencies=[Depends(require_user)])
 api_router.include_router(contacts_router, dependencies=[Depends(require_user)])
+api_router.include_router(contact_fields_router, dependencies=[Depends(require_user)])
 api_router.include_router(settings_router, dependencies=[Depends(require_user)])
 api_router.include_router(users_router, dependencies=[Depends(require_user)])
