dadc211aeef887486ed5182fe1ce8de77890a702
ShotOnMedia <shotonmedia@gmail.com>
2026-08-21 21:11:24 +0200

    Register contacts API

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from app.api.auth import router as auth_router
+from app.api.contacts import router as contacts_router
 from app.api.conversations import router as conversations_router
 from app.api.settings import router as settings_router
 from app.api.users import router as users_router
@@ -11,5 +12,6 @@ api_router = APIRouter()
 api_router.include_router(auth_router)
 api_router.include_router(webhooks_router)
 api_router.include_router(conversations_router, dependencies=[Depends(require_user)])
+api_router.include_router(contacts_router, dependencies=[Depends(require_user)])
 api_router.include_router(settings_router, dependencies=[Depends(require_user)])
 api_router.include_router(users_router, dependencies=[Depends(require_user)])
