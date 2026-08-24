2387e591fe31e846dffed9ff49f1fc62618d4b61
ShotOnMedia <shotonmedia@gmail.com>
2026-08-24 15:28:43 +0200

    Show flows library before opening visual builder

--- frontend/src/FlowBuilder.vue
+++ frontend/src/FlowBuilder.vue
@@ -1,9 +1,17 @@
 <script setup>
+import { ref } from 'vue'
+import FlowsLibrary from './FlowsLibrary.vue'
 import VisualFlowBuilder from './VisualFlowBuilder.vue'
 
-defineProps({ currentUser:{type:Object,required:true} })
+const props=defineProps({currentUser:{type:Object,required:true}})
+const editingFlowId=ref(null)
+const createNew=ref(false)
+function editFlow(id){editingFlowId.value=Number(id);createNew.value=false}
+function createFlow(){editingFlowId.value=null;createNew.value=true}
+function backToLibrary(){editingFlowId.value=null;createNew.value=false}
 </script>
 
 <template>
-  <VisualFlowBuilder :current-user="currentUser" />
+  <VisualFlowBuilder v-if="editingFlowId||createNew" :current-user="currentUser" :initial-flow-id="editingFlowId" :create-new="createNew" @back="backToLibrary"/>
+  <FlowsLibrary v-else @edit="editFlow" @create="createFlow"/>
 </template>
