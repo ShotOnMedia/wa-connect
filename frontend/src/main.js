31284c131cc7fd6aac0163eb5f7561aede7e2ed9
ShotOnMedia <shotonmedia@gmail.com>
2026-08-25 14:23:26 +0200

    Install live chat contact extras

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,7 @@ import Root from './Root.vue'
 import './style.css'
 import './inbox-layout.css'
 import './auth.css'
+import { installLiveChatExtras } from './live-chat-extras'
 
 createApp(Root).mount('#app')
+installLiveChatExtras()
