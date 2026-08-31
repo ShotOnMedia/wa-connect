c479838b1e69d9b49deb93546ac203a923f1768d
ShotOnMedia <shotonmedia@gmail.com>
2026-08-31 14:07:15 +0200

    Register Telegram API routes

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -6,6 +6,8 @@ from app.api.contacts import router as contacts_router
 from app.api.conversations import router as conversations_router
 from app.api.flows import router as flows_router
 from app.api.settings import router as settings_router
+from app.api.telegram import router as telegram_router
+from app.api.telegram_webhooks import router as telegram_webhooks_router
 from app.api.users import router as users_router
 from app.api.webhooks import router as webhooks_router
 from app.core.security import require_user
@@ -13,9 +15,11 @@ from app.core.security import require_user
 api_router = APIRouter()
 api_router.include_router(auth_router)
 api_router.include_router(webhooks_router)
+api_router.include_router(telegram_webhooks_router)
 api_router.include_router(conversations_router, dependencies=[Depends(require_user)])
 api_router.include_router(contacts_router, dependencies=[Depends(require_user)])
 api_router.include_router(contact_fields_router, dependencies=[Depends(require_user)])
 api_router.include_router(flows_router, dependencies=[Depends(require_user)])
 api_router.include_router(settings_router, dependencies=[Depends(require_user)])
+api_router.include_router(telegram_router)
 api_router.include_router(users_router, dependencies=[Depends(require_user)])
