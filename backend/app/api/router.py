670d3da9729f652ca81cd68053d7d13349cb0592
ShotOnMedia <shotonmedia@gmail.com>
2026-09-01 10:25:51 +0200

    Register HTTP API management routes

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -5,6 +5,7 @@ from app.api.contact_fields import router as contact_fields_router
 from app.api.contacts import router as contacts_router
 from app.api.conversations import router as conversations_router
 from app.api.flows import router as flows_router
+from app.api.http_apis import router as http_apis_router
 from app.api.settings import router as settings_router
 from app.api.telegram import router as telegram_router
 from app.api.telegram_webhooks import router as telegram_webhooks_router
@@ -20,6 +21,7 @@ api_router.include_router(conversations_router, dependencies=[Depends(require_us
 api_router.include_router(contacts_router, dependencies=[Depends(require_user)])
 api_router.include_router(contact_fields_router, dependencies=[Depends(require_user)])
 api_router.include_router(flows_router, dependencies=[Depends(require_user)])
+api_router.include_router(http_apis_router)
 api_router.include_router(settings_router, dependencies=[Depends(require_user)])
 api_router.include_router(telegram_router)
 api_router.include_router(users_router, dependencies=[Depends(require_user)])
