fa5e5dc604b72792ae68d4bb0fd14eaffc53c4fc
ShotOnMedia <shotonmedia@gmail.com>
2026-09-09 09:24:50 +0200

    Enable live chat paging and multi-trigger editor

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,7 +7,11 @@ import './http-response-mapping.css'
 import './campaign-question-modal.css'
 import { installLiveChatExtras } from './live-chat-extras'
 import { installTelegramContactExtras } from './telegram-contact-extras'
+import { installLiveChatPagination } from './live-chat-pagination'
+import { installFlowTriggerExtras } from './flow-trigger-extras'
 
 createApp(Root).mount('#app')
 installLiveChatExtras()
 installTelegramContactExtras()
+installLiveChatPagination()
+installFlowTriggerExtras()
