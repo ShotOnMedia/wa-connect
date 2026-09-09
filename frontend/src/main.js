6c57dc2a68812d306dcacaacdaf0e77a89850f92
ShotOnMedia <shotonmedia@gmail.com>
2026-09-09 06:32:56 +0200

    Install Telegram shared contact renderer

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,8 @@ import './auth.css'
 import './http-response-mapping.css'
 import './campaign-question-modal.css'
 import { installLiveChatExtras } from './live-chat-extras'
+import { installTelegramContactExtras } from './telegram-contact-extras'
 
 createApp(Root).mount('#app')
 installLiveChatExtras()
+installTelegramContactExtras()
