bd01a47ee6522d99eba8a93c16684f96f54c31be
ShotOnMedia <shotonmedia@gmail.com>
2026-09-11 13:33:42 +0200

    Install live chat display controls

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,12 +5,14 @@ import './inbox-layout.css'
 import './auth.css'
 import './http-response-mapping.css'
 import './campaign-question-modal.css'
+import { installLiveChatDisplay } from './live-chat-display'
 import { installLiveChatExtras } from './live-chat-extras'
 import { installTelegramContactExtras } from './telegram-contact-extras'
 import { installLiveChatPagination } from './live-chat-pagination'
 import { installFlowTriggerExtras } from './flow-trigger-extras'
 import { installTelegramCommands } from './telegram-commands-mount'
 
+installLiveChatDisplay()
 createApp(Root).mount('#app')
 installLiveChatExtras()
 installTelegramContactExtras()
